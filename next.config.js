7da310f3c706dbd013fc70ec5d51567c251df822
Mikkel Kidde Thomsen <45518395+mKiddeT@users.noreply.github.com>
2022-03-16 15:39:33 +0100

    Site now works as a PWA (#4)
    * Site now works as a PWA
    
    * deleted autogenerated files
    
    * updated .gitignore to ignore autogenerated files when compiling

--- next.config.js
+++ next.config.js
@@ -4,3 +4,13 @@ const nextConfig = {
 }
 
 module.exports = nextConfig
+
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+});
