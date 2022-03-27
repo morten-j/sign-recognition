6b87ec4929b2ed17790a6614f704c47db0edc093
Morten <mjarg19@student.aau.dk>
2022-03-27 14:08:13 +0200

    setup + lidt hygge

---
+++ sanic/src/keras_class_test.py
@@ -0,0 +1,30 @@
+from tensorflow import keras
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+
+def readucr(filename):
+    data = np.loadtxt(filename, delimiter="\t")
+    y = data[:, 0]
+    x = data[:, 1:]
+    return x, y.astype(int)
+
+
+root_url = "https://raw.githubusercontent.com/hfawaz/cd-diagram/master/FordA/"
+
+x_train, y_train = readucr(root_url + "FordA_TRAIN.tsv")
+x_test, y_test = readucr(root_url + "FordA_TEST.tsv")
+
+
+classes = np.unique(np.concatenate((y_train, y_test), axis=0))
+
+plt.figure()
+for c in classes:
+    c_x_train = x_train[y_train == c]
+    plt.plot(c_x_train[0], label="class " + str(c))
+plt.legend(loc="best")
+plt.show()
+plt.close()
+
+
