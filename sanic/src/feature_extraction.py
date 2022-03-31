4b387fd060e044b2a9a4652bab80b190e4df4d16
Casper Kjærhus Leth <56830573+CasperKjaerhus@users.noreply.github.com>
2022-03-31 09:44:30 +0200

    cleaned up code

--- sanic/src/feature_extraction.py
+++ sanic/src/feature_extraction.py
@@ -21,13 +21,14 @@ def getImagesFromVideo(video):
     videoWrapper.release()
 
 def getLandmarksFromVideo(video, flipped=False):
-    results = []
+
+    frames = []
+
     with mp_hands.Hands(
         model_complexity=0,
         max_num_hands=2,
         min_detection_confidence=0.5,
         min_tracking_confidence=0.5) as handtracker:
-        
         
         for image in getImagesFromVideo(video):
             if flipped:
@@ -36,13 +37,13 @@ def getLandmarksFromVideo(video, flipped=False):
             result = handtracker.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
             hands = {'left': [], 'right': []}
+
             for index, hand_landmarks in enumerate(result.multi_hand_landmarks):
                 handedness = 'left' if index == 0 else "right"
 
                 for landmark in hand_landmarks.landmark:
                     hands[handedness].append({'x': landmark.x, 'y': landmark.y, 'z': landmark.z})
                 
-            results.append(hands)
+            frames.append(hands)
 
-        print(f'Frame 1, left-hand, wrist: {results[0]["left"][0]}')
-        return results
+        return frames
