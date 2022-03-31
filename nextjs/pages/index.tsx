952fa1915bc768312fbce345ce1a63bfdfd5eeae
mKiddeT <mth19@student.aau.dk>
2022-03-31 09:35:40 +0200

    small css fixes

--- nextjs/pages/index.tsx
+++ nextjs/pages/index.tsx
@@ -7,15 +7,18 @@ export default function LearningPage() {
 
   return (
     <>
-      <div>
-        { showWebcam ? null : <VideoPlayer /> }
-        { showWebcam ? <WebcamCapture /> : null }
-      </div>
-    
-      <div className="p-6 mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4 w-2/3" >
-        {/* buttons */}
-        { showWebcam ? null : <button onClick={() => setShowWebcam(true)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Record</button> }
-        { showWebcam ? <button onClick={() => setShowWebcam(false)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Back to vid</button> : null }
+      <div className="p-6 mx-auto bg-white rounded-xl shadow-lg flex-col w-2/3">
+        <h1 className="">suh</h1>
+        <div className="">
+          { showWebcam ? null : <VideoPlayer /> }
+          { showWebcam ? <WebcamCapture /> : null }
+        </div>
+      
+        <div className="" >
+          {/* buttons */}
+          { showWebcam ? null : <button onClick={() => setShowWebcam(true)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Record</button> }
+          { showWebcam ? <button onClick={() => setShowWebcam(false)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Back to vid</button> : null }
+        </div>
       </div>
     </>
   );
