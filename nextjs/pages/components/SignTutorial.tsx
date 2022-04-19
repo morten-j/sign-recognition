b0a6a1c5f61fca571fa24b6bc64899be4e21b879
Casper Kjærhus Leth <56830573+CasperKjaerhus@users.noreply.github.com>
2022-04-19 11:11:30 +0200

    added SignTutorial modal
    and cleaned up index.tsx

---
+++ nextjs/pages/components/SignTutorial.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react'
+import VideoPlayer from './VideoPlayer'
+
+type Props = {
+    signName: String,
+    show: boolean,
+    closeModal: () => void,
+}
+
+const SignTutorial = ({signName, show, closeModal}: Props) => {
+
+    const styling = 'w-full h-full bg-gray-900 bg-opacity-80 top-0 fixed sticky-0 z-50'
+
+    return (
+    <div className={show ? styling : styling + " hidden"}>
+        <div className="2xl:container  2xl:mx-auto py-48 px-4 md:px-28 flex justify-center items-center">
+            <div className="mt-12 w-96 md:w-auto dark:bg-gray-800 relative flex flex-col justify-center items-center bg-white py-16 px-4 md:px-24 xl:py-24 xl:px-36 gap-5">
+
+                <button onClick={closeModal} className="text-gray-800 dark:text-gray-400 absolute top-8 right-8 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-800">
+                    <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M18 6L6 18" stroke="currentColor" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round" />
+                        <path d="M6 6L18 18" stroke="currentColor" stroke-width="1.66667" stroke-linecap="round" stroke-linejoin="round" />
+                    </svg>
+                </button>
+
+                <h1 className="text-3xl dark:text-white lg:text-4xl font-semibold leading-7 lg:leading-9 text-center text-gray-800">
+                    Make this sign: {signName}
+                </h1>
+
+                <VideoPlayer url="signs/signvid.webm" />
+
+                <button onClick={closeModal} className="bg-blue-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
+                    I'm Ready!
+                </button>
+
+            </div>
+        </div>
+        
+    </div>
+    )
+}
+
+export default SignTutorial
--- nextjs/pages/index.tsx
+++ nextjs/pages/index.tsx
@@ -1,25 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import VideoPlayer from "./components/VideoPlayer";
 import WebcamCapture from "./components/WebcamCapture";
+import SignTutorial from "./components/SignTutorial";
 
 export default function LearningPage() {
-  const [showWebcam, setShowWebcam] = React.useState(false)
+    const [showWebcam, setShowWebcam] = useState(false)
 
-  return (
-    <>
-      <div className="p-6 mx-auto bg-white rounded-xl shadow-lg flex flex-col w-2/3">
-        <h1 className="text-center">ASL recognizer</h1>
-        <div className="self-center">
-          { showWebcam ? null : <VideoPlayer /> }
-          { showWebcam ? <WebcamCapture /> : null }
-        </div>
-      
-        <div className="self-center" >
-          {/* buttons */}
-          { showWebcam ? null : <button onClick={() => setShowWebcam(true)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Record</button> }
-          { showWebcam ? <button onClick={() => setShowWebcam(false)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">Back to vid</button> : null }
-        </div>
-      </div>
-    </>
-  );
+    const [show, setShow] = useState(false);
+
+    const closeModal = () => setShow(false);
+    const showModal = () => setShow(true);
+
+    return (
+        <>
+            <SignTutorial signName={"Pizza"} show={show} closeModal={closeModal} />
+            <div className="p-6 mx-auto bg-white rounded-xl shadow-lg flex flex-col w-2/3">
+                <h1 className="text-center">ASL recognizer</h1>
+                <div className="self-center">
+                    {showWebcam ? <WebcamCapture /> : <VideoPlayer url="sign_videos/signvid.webm" />}
+                </div>
+
+                <div className="self-center" >
+                    <button onClick={() => setShowWebcam(!showWebcam)} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                        {showWebcam ? "Back to vid" : "Record"}
+                    </button>
+                </div>
+            </div>
+        </>
+    );
 }
