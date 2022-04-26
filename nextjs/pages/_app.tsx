1c87236fd400baeac5f18818bdf9f46b25c4ee1a
Casper Kjærhus Leth <56830573+CasperKjaerhus@users.noreply.github.com>
2022-04-26 11:59:52 +0200

    Prompt with sign tutorial (#13)
    * changed VideoPlayer to arrow component
    
    and gave it props
    
    * Give videoplayer a url prop
    
    * added SignTutorial modal
    
    and cleaned up index.tsx
    
    * index pretty now
    
    * reformatted to 4 spaces
    
    * added custom keypress hook
    
    * Removed VideoPlayer component
    
    * Switched to conditional rendering
    
    * Added Signs associated with videos
    
    * Prompt functionality now in functional state
    
    * Cleanup and changes
    
    * Fixed qoutes
    
    * Fixed type String --> string
    
    * formatted to 4 spaces
    
    Co-authored-by: Dino <25507425+Deaxz@users.noreply.github.com>

--- nextjs/pages/_app.tsx
+++ nextjs/pages/_app.tsx
@@ -3,26 +3,26 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return (
-    <>
-      <Head>
-        <meta charSet="utf-8" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
-        <meta
-          name="viewport"
-          content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
-        />
-        <meta name="description" content="Description" />
-        <meta name="keywords" content="Keywords" />
-        <title>Next.js PWA Example</title>
+    return (
+        <>
+            <Head>
+                <meta charSet="utf-8" />
+                <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+                <meta
+                    name="viewport"
+                    content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+                />
+                <meta name="description" content="Description" />
+                <meta name="keywords" content="Keywords" />
+                <title>Next.js PWA Example</title>
 
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icon.png"></link>
-        <meta name="theme-color" content="#fff" />
-      </Head>
-      <Component {...pageProps} />
-    </>
-  )
+                <link rel="manifest" href="/manifest.json" />
+                <link rel="apple-touch-icon" href="/icon.png"></link>
+                <meta name="theme-color" content="#fff" />
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default MyApp
