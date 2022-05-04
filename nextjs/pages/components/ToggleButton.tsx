9c003197ba3719c8596a4d78149950aa2d85efd5
Casper Kjærhus Leth <56830573+CasperKjaerhus@users.noreply.github.com>
2022-05-04 09:51:48 +0200

    renamed setIsToggled
    Co-authored-by: Mikkel Kidde Thomsen <45518395+mKiddeT@users.noreply.github.com>

--- nextjs/pages/components/ToggleButton.tsx
+++ nextjs/pages/components/ToggleButton.tsx
@@ -12,7 +12,7 @@ function ToggleButton({ isToggled, setIsToggled, label }: props) {
         <div className="flex items-center justify-center">
             <label htmlFor="toogleA" className="flex items-center cursor-pointer">
                 <span className="relative">
-                    <input id="toogleA" type="checkbox" className="sr-only" checked={isToggled} onChange={() => setisToggled(!isToggled)}/>
+                    <input id="toogleA" type="checkbox" className="sr-only" checked={isToggled} onChange={() => setIsToggled(!isToggled)}/>
                     <div className="w-10 h-4 bg-gray-400 rounded-full shadow-inner"></div>
                     <div className="dot absolute w-6 h-6 bg-white rounded-full shadow -left-1 -top-1 transition"></div>
                 </span>
